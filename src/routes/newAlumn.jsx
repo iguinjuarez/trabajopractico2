4be89a022b245d9c82a9db828deb15cf8a18be20
iguinjuarez <iguin632@gmail.com>
2024-11-09 19:49:42 -0300

    Update newAlumn.jsx

--- src/routes/newAlumn.jsx
+++ src/routes/newAlumn.jsx
@@ -1,7 +1,13 @@
 import { useNavigate } from "react-router-dom";
+import { useForm } from 'react-hook-form';
 
 export default function NewAlumn() {
   const navigate = useNavigate();
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = (data) => {
+    alert(`Formulario enviado: ${JSON.stringify(data)}`);
+};
+
     return (
       <>
         <div class="page-student">
@@ -14,14 +20,38 @@ export default function NewAlumn() {
             Atrás
           </button>
             </div>
-            <form onSubmit={(event) => addInfo(event)}>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div class="form-add">                   
                     <label for="nombre">Nombre:</label>
-                    <input type="text" id="name" name="name" required/> 
+                    <input {...register("nombre", { required: "El nombre es obligatorio" })} />
+        {errors.nombre && <p>{errors.nombre.message}</p>}
                 </div>
                 <div class="form-add">
                     <label for="apellido">Apellido:</label>
-                    <input type="text" id="lastName" name="lastName" required/>       
+                    <input {...register("apellido", { required: "El apellido es obligatorio" })} />
+        {errors.apellido && <p>{errors.apellido.message}</p>}      
+                </div>
+                <div class="form-add">
+                    <label for="dni">DNI:</label>
+                    <input {...register("dni", { 
+                    required: "El DNI es obligatorio", 
+                    pattern: { 
+                        value:  /^[0-9]{1,10}$/,
+                        message: "DNI invalido"
+                    }
+                })} />
+        {errors.dni && <p>{errors.dni.message}</p>}      
+                </div>
+                <div class="form-add">
+                    <label for="mail">E-mail:</label>
+                    <input {...register("mail", { 
+                    required: "El E-mail es obligatorio", 
+                    pattern: { 
+                        value:  /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                        message: "E-mail inválido"
+                    }
+                })} />
+        {errors.mail && <p>{errors.mail.message}</p>}      
                 </div>
                 <div class="form-add">
                     <button class="button-nav-add" type="submit">Agregar</button>
